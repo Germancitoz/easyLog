ef4c986061e0092cbbb065d8cf37ba7ba0643218
Germancito <germanfernandez0306@gmail.com>
2021-11-12 19:51:51 -0300

    feat(sendLog:date): new possibility to add date

--- src/modules/log.js
+++ src/modules/log.js
@@ -3,15 +3,20 @@ import { saveFile } from "./file.js";
 import COLORS from "../config/colors.js";
 
 export const sendLog = (type, message, options) => {
-  const { path, showInConsole } = options;
+  const { path, showInConsole, useDate } = options;
 
   let template = `[${type}] - ${message}`;
+
+  if (useDate) {
+    const date = new Date();
+    template = `${date.toLocaleDateString()} | ${template}`;
+  }
 
   saveFile(path, type, template);
 
   if (showInConsole) {
     const color = COLORS[type];
-    template = `${color}[${type}]${COLORS.RESET} - ${message}`;
+    template = `${color}${date} | [${type}]${COLORS.RESET} - ${message}`;
     console.log(template);
   }
 };
