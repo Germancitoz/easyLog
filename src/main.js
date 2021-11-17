0cb5784c53be0b29903a324509ceb2d8b08dea8d
Germancito <germanfernandez0306@gmail.com>
2021-11-17 18:39:22 -0300

    feat(main): add new methods (debug, color)

--- src/main.js
+++ src/main.js
@@ -11,5 +11,8 @@ const logger = {
 logger.done = (message) => sendLog("DONE", message, logger.options);
 logger.error = (message) => sendLog("ERROR", message, logger.options);
 logger.warn = (message) => sendLog("WARN", message, logger.options);
+logger.debug = (message) => sendLog("DEBUG", message, logger.options);
+
+logger.color = (color, message) => sendLog(color, message, logger.options);
 
 export default logger;
