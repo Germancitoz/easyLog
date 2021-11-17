9e7b9a7be818f5782587b58da9441453d93ae9be
Germancito <germanfernandez0306@gmail.com>
2021-11-17 18:37:30 -0300

    refactor(config): change name of config colors to style

--- src/config/colors.js
+++
@@ -1,7 +0,0 @@
-const COLORS = {
-  RESET: "\x1b[0m",
-  ERROR: "\x1b[31m",
-  DONE: "\x1b[32m",
-  WARN: "\x1b[33m",
-};
-export default COLORS;
---
+++ src/config/style.js
@@ -0,0 +1,13 @@
+const STYLE = {
+  RESET: "\x1b[0m",
+  ERROR: "\x1b[31m",
+  DONE: "\x1b[32m",
+  WARN: "\x1b[33m",
+  DEBUG: "\x1b[35m",
+
+  RED: "\x1b[31m",
+  GREEN: "\x1b[32m",
+  YELLOW: "\x1b[33m",
+  BLUE: "\x1b[34m",
+};
+export default STYLE;
