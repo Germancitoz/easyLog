6d04c373ad866d326fafd6aef10f93022ae18de3
Germancito <germanfernandez0306@gmail.com>
2021-11-17 18:38:58 -0300

    refactor(file): change path format

--- src/modules/file.js
+++ src/modules/file.js
@@ -2,7 +2,7 @@ import fs from "node:fs";
 
 export const saveFile = (path, type, data) => {
   const date = new Date();
-  path = path + `/${date.getDay()}-${date.getMonth()}`;
+  path = `${path}/${date.getDay()}-${date.getMonth()}`;
 
   if (!fs.existsSync(path)) {
     fs.mkdirSync(path, {
